1fa14ab92dfb57604fc4ec4ccf8e9c9e43322dcf
Abd Aljaleel EL Baba <aboodbaba2000@hotmail.com>
2020-12-30 22:52:07 +0200

    make new space

--- app/Http/Controllers/Api/V1/Users/UserController.php
+++ app/Http/Controllers/Api/V1/Users/UserController.php
@@ -24,6 +24,7 @@ class UserController extends Controller
         {
             $passHash = Hash::make($request->password);
         }
+
         User::create(array_merge($request->except('password'), ['password' => $passHash]));
         return ['successes' => 'the user has been created'];
     }
